af54653cfa6ff73d716f0272fef0ed345b992693
Samuel Kadolph <samuel@kadolph.com>
2012-10-07 22:04:00 -0400

    Bump trash formula to 1.0.1

--- Library/Formula/trash.rb
+++ Library/Formula/trash.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class Trash < Formula
   homepage "https://github.com/samuelkadolph/trash"
-  url "https://github.com/samuelkadolph/trash/tarball/v1.0.0"
-  sha1 "ab3298f330aedf80e734d2979e26f9dcbf6e3fa5"
+  url "https://github.com/samuelkadolph/trash/tarball/v1.0.1"
+  sha1 "fcb28e16006038224550ea8df9e760537177549e"
 
   head "https://github.com/samuelkadolph/trash"
 
